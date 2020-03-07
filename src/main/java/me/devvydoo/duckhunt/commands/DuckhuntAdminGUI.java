bf4fd16ca755ecc4ed15e9f31bcda880918e2483
Devvy <devinhall4@gmail.com>
2020-03-07 04:40:16 -0500

    update version

--- src/main/java/me/devvydoo/duckhunt/commands/DuckhuntAdminGUI.java
+++ src/main/java/me/devvydoo/duckhunt/commands/DuckhuntAdminGUI.java
@@ -1,6 +1,5 @@
 package me.devvydoo.duckhunt.commands;
 
-import com.sun.istack.internal.NotNull;
 import me.devvydoo.duckhunt.game.DuckhuntGame;
 import me.devvydoo.duckhunt.round.WaitingRound;
 import org.bukkit.Bukkit;
@@ -11,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -38,7 +36,6 @@ public class DuckhuntAdminGUI implements Listener, InventoryHolder {
         initInterface();
     }
 
-    @NotNull
     @Override
     public Inventory getInventory() {
         return inventory;
